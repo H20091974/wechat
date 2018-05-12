d59b703dc42bcbb4899c600550ba1690c00adfbb
zhenzhenclaire7@gmail.com <zhenzhen090647>
2018-05-12 22:16:51 +0800

    测试回发消息

--- src/main/java/com/claire7/wechat/controller/WeChatController.java
+++ src/main/java/com/claire7/wechat/controller/WeChatController.java
@@ -9,6 +9,8 @@ import com.soecode.wxtools.api.WxMessageRouter;
 import com.soecode.wxtools.api.WxService;
 import com.soecode.wxtools.bean.WxXmlMessage;
 import com.soecode.wxtools.bean.WxXmlOutMessage;
+import com.soecode.wxtools.bean.WxXmlOutNewsMessage;
+import com.soecode.wxtools.bean.outxmlbuilder.NewsBuilder;
 import com.soecode.wxtools.util.xml.XStreamTransformer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,7 +55,7 @@ public class WeChatController {
                     .eventKey(Constant.MenuKey.TALK_ABOUT).handler(TalkAboutDesignHandler.getInstance()).end();
 
             // 把消息传递给路由器进行处理
-            WxXmlOutMessage xmlOutMsg = router.route(wx);
+            WxXmlOutMessage xmlOutMsg = execute(wx);
             if (xmlOutMsg != null)
                 out.print(xmlOutMsg.toXml());
         } catch (Exception e) {
@@ -62,6 +64,22 @@ public class WeChatController {
             out.close();
         }
 
+    }
+
+
+    private WxXmlOutMessage execute(WxXmlMessage wxMessage) {
+        try {
+            NewsBuilder newsBuilder = WxXmlOutMessage.NEWS();
+            WxXmlOutNewsMessage.Item item = new WxXmlOutNewsMessage.Item();
+            item.setTitle("当小清新遇见盛世古城，月坨岛与滦州的两日之旅（下）");
+            item.setDescription("短短的京郊两日游就这样结束啦，旅程虽短，给心灵放个假。下次，我们再会。");
+            item.setUrl("https://mp.weixin.qq.com/s/L5xNkvYKKR8qurLeYe3pEQ");
+            item.setPicUrl("https://mmbiz.qpic.cn/mmbiz_jpg/ArpjhGmjianziaPCS7sGbRJHibq6YgYDtfOobc4roMibRvrGyrtB4oxZickcP8ZLGZOibh0IvtZpy3A7tjSZcl0uicicrA/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1");
+            newsBuilder.addArticle(item);
+            return newsBuilder.toUser(wxMessage.getFromUserName()).fromUser(wxMessage.getToUserName()).build();
+        } catch (Exception e) {
+        }
+        return WxXmlOutMessage.TEXT().content(Constant.ResponseConst.DEFAULE_TEXT).toUser(wxMessage.getFromUserName()).fromUser(wxMessage.getToUserName()).build();
     }
 
 }
